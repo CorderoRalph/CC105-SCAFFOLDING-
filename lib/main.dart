63bfaa7b6686aef549f9bf08abfe007ed6fd6e78
CORDERO <sm201600568@wmsu.edu.ph>
2020-11-05 00:45:13 +0800

    asset

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
             ),
             body: Center(
               child: Image(
-                  image: NetworkImage('https://i.pinimg.com/originals/43/ec/64/43ec643e415690208f5d933cf9dcef84.jpg')),
+                  image:AssetImage('images/diamond.png')),
             )
         ),
       )
